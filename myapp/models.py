89ec76e97449692ae9ffc9ce5da02474f7db0ab4
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-22 05:48:22 -0700

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,6 @@ Base = declarative_base
 
 class SMSMessage(Base):
     __tablename__ = 'sms_messages'
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(500))
-    result = db.Column(db.String(20))
+    id = Column(Integer, primary_key=True)
+    text = Column(String(500))
+    result = Column(String(20))
