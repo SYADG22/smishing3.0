b53bfd28979c16a18cb87e5c7c2f688e56da77f7
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-22 08:43:42 -0700

    Update extensions.py

--- myapp/extensions.py
+++ myapp/extensions.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 #from .app import main
 
 db = create_engine('postgresql://sms_messages_user:leMmtPVKr4TFxdudP6hYBPWPSnKRtr7Q@dpg-ck624kgs0i2c73chms00-a.oregon-postgres.render.com/sms_messages')
 
 Session = sessionmaker(bind=db)
+
+db_session = scoped_session(Session)
