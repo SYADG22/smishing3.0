c18bb98fdb0c388d43fbc64191cc6301e2a12c11
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-22 05:54:44 -0700

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -2,10 +2,11 @@ from .extensions import db
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-Base = declarative_base
+Base = declarative_base()
 
 class SMSMessage(Base):
     __tablename__ = 'sms_messages'
+    
     id = Column(Integer, primary_key=True)
     text = Column(String(500))
     result = Column(String(20))
