64e6bbf7bfae926b9282e8c6680c339c5cc70236
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-22 06:32:20 -0700

    Update __init__.py

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -8,7 +8,7 @@ from .app import main
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://sms_messages_user:leMmtPVKr4TFxdudP6hYBPWPSnKRtr7Q@dpg-ck624kgs0i2c73chms00-a.oregon-postgres.render.com/sms_messages'
+    #app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://sms_messages_user:leMmtPVKr4TFxdudP6hYBPWPSnKRtr7Q@dpg-ck624kgs0i2c73chms00-a.oregon-postgres.render.com/sms_messages'
     # postgres://prettyprinted_render_example_user:11vq6k72GmFJazhVpz3pFUko50djVZT1@dpg-ceukdhmn6mpglqdb4avg-a.oregon-postgres.render.com/prettyprinted_render_example
 
     #db.init_app(app)
