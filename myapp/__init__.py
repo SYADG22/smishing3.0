6826943f930e4cdc70eeff16d8b21f649e581f2d
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-22 06:18:49 -0700

    Update __init__.py

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://sms_messages_user:leMmtPVKr4TFxdudP6hYBPWPSnKRtr7Q@dpg-ck624kgs0i2c73chms00-a.oregon-postgres.render.com/sms_messages'
     # postgres://prettyprinted_render_example_user:11vq6k72GmFJazhVpz3pFUko50djVZT1@dpg-ceukdhmn6mpglqdb4avg-a.oregon-postgres.render.com/prettyprinted_render_example
 
-    db.init_app(app)
+    #db.init_app(app)
 
     app.register_blueprint(main)
 
