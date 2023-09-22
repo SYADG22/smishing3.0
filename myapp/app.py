e4ccbceb182b8813dbeb125bd36bddeb29fe2159
SYADG22 <95881369+SYADG22@users.noreply.github.com>
2023-09-22 06:49:27 -0700

    Update app.py

--- myapp/app.py
+++ myapp/app.py
@@ -80,7 +80,8 @@ def predict():
         db_msg = 'smishing'
     else:
         db_msg = 'legit'
-         '''
+         
+     '''
     new_message = SMSMessage(text=msg, result=db_msg)
     session.add(new_message)
     session.commit()
